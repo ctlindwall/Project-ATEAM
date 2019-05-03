7ad22bc47f92969f21f085d88151fefd736bd378
Turner Valle <47796022+turnervalle@users.noreply.github.com>
2019-05-03 15:12:38 -0500

    Add files via upload

--- Node.java
+++ Node.java
@@ -1,37 +1,5 @@
+package application;
 
-public class Node<Question>{
-	private Question value;
-	private Node<Question> next;
-	private boolean answered;
-	public Node(Question q) {
-		value = q;
-		this.next = null;
-		answered = false;
-	}
-	
-	public Node(Question q, Node<Question> next) {
-		value = q;
-		this.next = next;
-		answered = false;
-	}
-	
-	public Node<Question> getNext() {
-		return next;
-	}
+public class Node {
 
-	public void setNext(Node<Question> next) {
-		this.next = next;
-	}
-	
-	public Question getValue() {
-		return value;
-	}
-	
-	public void setAnswered(boolean value) {
-		answered = value;
-	}
-	
-	public boolean getAnswered() {
-		return answered;
-	}
 }
